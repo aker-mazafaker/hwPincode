89a82df08ca3ae5c5bad8773cd5d0ff8981803e2
Aker <@akerke.n@inbox.ru>
2023-06-27 15:16:34 +0600

    functionality

--- hwPincode/ViewController.swift
+++ hwPincode/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    var pinCode = ""
+    let correctPincode = 2809
+    let maxPinLength = 4
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var buttonDelete: UIButton!
+    
+    
+    @IBOutlet weak var circle1: UIImageView!
+    @IBOutlet weak var circle2: UIImageView!
+    @IBOutlet weak var circle3: UIImageView!
+    @IBOutlet weak var circle4: UIImageView!
+    
+  override  func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func numberButton(_ sender: Any) {
+        if let button = sender as? UIButton {
+            let tag = button.tag
+            circle1.image = UIImage(systemName: "circle.fill")
+            print("кнопка\(tag)")
+        }
+    }
 }
 
